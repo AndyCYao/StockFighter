e6af87ccdc8b5ccc4d1b304c08b404cbc4564d20
Andy Y <andyyao3@gmail.com>
2016-02-25 17:36:15 -0800

    reworked thirdTrade

--- gamemaster.py
+++ gamemaster.py
@@ -1,10 +1,14 @@
+"""Contains GameMaster and Stockfighter."""
 import requests
 import json
 
+
 class GameMaster:
+    """Contains the codes necessary to start the instance."""
+
     base_url = "https://api.stockfighter.io/ob/api"
     gm_url = "https://www.stockfighter.io/gm"
-    
+
     def __init__(self):
         pass
 
@@ -18,8 +22,8 @@ class GameMaster:
         pass
 
     def get_api_key(self):
-        file = open('apikey.txt','r')
-        apikey  = file.readlines()[0]
+        file = open('apikey.txt', 'r')
+        apikey = file.readlines()[0]
         return apikey
 
     def get_instance_id(self):
@@ -34,7 +38,7 @@ class GameMaster:
         except ValueError as e:
             return{'error': e, 'raw_content': response.content}
 
-    def post_level(self,level):
+    def post_level(self, level):
         header = {'X-Starfighter-Authorization': self.get_api_key()}
         full_url = "%s/levels/%s" % (self.gm_url, level)
         response = requests.post(full_url, headers=header)
@@ -45,17 +49,14 @@ class GameMaster:
             return{'error': e, 'raw_content': response.content}
 
 
-
 class StockFighter:
-    
-    
-    
-    def __init__(self,LevelName):
+    """Contains the methods neccessary to make API calls."""
 
+    def __init__(self, LevelName):
         gm = GameMaster()
         apikey = gm.get_api_key()
-        response =  gm.post_level(LevelName)
-        
+        response = gm.post_level(LevelName)
+
         self.base_url = "https://api.stockfighter.io/ob/api"
         self.account = response.json().get("account")
         self.instanceID = response.json().get("instanceId")
@@ -66,35 +67,36 @@ class StockFighter:
         # further research at https://discuss.starfighters.io/t/the-gm-api-how-to-start-stop-restart-resume-trading-levels-automagically/143
 
     def get_order_book(self, s):
-        full_url = "%s/venues/%s/stocks/%s" %(self.base_url,self.venues,s)
+        """retrieve the order book"""
+        full_url = "%s/venues/%s/stocks/%s" % (self.base_url, self.venues, s)
         response = requests.get(full_url, headers=self.header)
         return response
 
-    def return_best_price(self, oBook):
+    def read_orderbook(self, oBook, direction, type):
         try:
-            BestAsk = oBook.json().get('asks')[0].get('price')
-            BestAskQty = oBook.json().get("asks")[0].get('qty')
-        except (RuntimeError, TypeError, NameError): 
-            BestAsk = 0
-            BestAskQty = 0
-        
-        return BestAsk, BestAskQty
+            best_result = oBook.json().get(direction)[0].get(type)
+        except (RuntimeError, TypeError, NameError):
+            best_result = 0
+        return best_result
 
-    def fill_confirmation(self, s,id):
-        full_url = "%s/venues/%s/stocks/%s/orders/%s" %(self.base_url,self.venues,s,id)
-        response = requests.get(full_url,headers=self.header)
+    def fill_confirmation(self, s, id):
+        """return total number filled for this specific id"""
+        full_url = "%s/venues/%s/stocks/%s/orders/%s" % (self.base_url, self.venues,s,id)
+        response = requests.get(full_url, headers=self.header)
         # print response.json()
-        return response.json().get('ok')
+        return response.json().get('totalFilled'), \
+            response.json().get('direction'), \
+            response.json().get('open')
 
-    def buy_stock(self, p, q, s):
+    def make_order(self, p, q, s, direction, orderType):
         order = {
             "account": self.account,
             "venue": self.venues,
             "symbol": s,
             "price": p,
             "qty": q,
-            "direction": "buy",
-            "orderType": "limit"
+            "direction": direction,
+            "orderType": orderType
         }
         full_url = "%s/venues/%s/stocks/%s/orders" % (self.base_url, self.venues, s)
         response = requests.post(full_url, headers=self.header, data=json.dumps(order))
