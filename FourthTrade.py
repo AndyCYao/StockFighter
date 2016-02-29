296d94b068816a66c49c3262ab65e934a0655c31
Andy Y <andyyao3@gmail.com>
2016-02-29 15:47:31 -0800

    Worked on Fourth Trade

--- FourthTrade.py
+++ FourthTrade.py
@@ -2,51 +2,43 @@ import gamemaster
 import time
 import json
 """
-Feb 25th 2016 Third Trade sell_side. Here is the approach currently.
-1.) script reads the most favorable quotes in the read_orderbook retrieve
-    their bid / ask / qty.
-2.) since the max long / short position is 1000, i cant make too risky trades.
-    so if the qty is > 100
-    i should just cap it at 100
-3.) Apply a mark up on the sell side to capture the premium
-4.) reguarly check the position using update_open_orders
-5.) Net Asset Value is the unrealized gain or loss of asset in hand
-    based on the diff. of current quote - purchased price
-Feb 29th 2016 -
-1.) add a way to check how many position pending ontop of the total filled.
+Feb 28th 2016 - Fourth Trade - Dueling Bull Dozers.
+
+Make $10,000 in profit while minimizing your exposure
+
 """
 
 
 start = time.time()
 end = time.time()
 
-sf = gamemaster.StockFighter("sell_side")
+sf = gamemaster.StockFighter("dueling_bulldozers")
 stock = sf.tickers
 
 positionSoFar = 0       # if negative means short if positive long total filled
-expectedPosition = 0    # this is if both filled and to be filled are accounted for (cant be over +/-1000)
+expectedPosition = 0    # this is if both filled and to be filled are accounted for (cant be over +/-1500)
 premium = 1.025
 ma_20_list = []    # moving average 20 lets the script know current trend.
 ma_20 = 0
 nav = 0
+orderIDList = 0
 
 
-def buy_condition(tSpread, tExpectedPosition):
+def buy_condition(tSpread, tExpectedPosition, tPositionSoFar):
     """to be tailored for each level."""
-    if tSpread > 0.005 and tExpectedPosition <= 0:
+    if tSpread > 0.005 and tExpectedPosition <= 0 and tPositionSoFar < 400:
         return True
     return False
 
 
-def sell_condition(tSpread, tExpectedPosition):
+def sell_condition(tSpread, tExpectedPosition, tPositionSoFar):
     """to be tailored for each level."""
-    if tSpread > 0.005 and tExpectedPosition >= 0:
+    if tSpread > 0.005 and tExpectedPosition >= 0 and tPositionSoFar > -400:
         return True
     return False
 
-# while (end - start) < 120:
 try:
-    while nav < 12000:
+    while nav < 250000:
         orderIDList = sf.status_for_all_orders_in_stock(stock)
         positionSoFar, cash, expectedPosition = sf.update_open_orders(orderIDList.json())
         oBook = sf.get_order_book(stock)
@@ -85,25 +77,23 @@ try:
 
         time.sleep(2)  # slow things down a bit, because we are querying the same information.
 
-        if buy_condition(Spread, expectedPosition):
+        if buy_condition(Spread, expectedPosition, positionSoFar):
             buyOrder = sf.make_order(BestBid, q_bid, stock, "buy", "limit")
             buyPrice = buyOrder.get('price')
             buyID = buyOrder.get('id')
-            print "placed buy ord. - %d units at %d ID %d" % (q_bid, buyPrice, buyID)
+            print "BBBBB placed buy ord. - %d units at %d ID %d" % (q_bid, buyPrice, buyID)
         
-        if sell_condition(Spread, expectedPosition):
+        if sell_condition(Spread, expectedPosition, positionSoFar):
             sellOrder = sf.make_order(int(BestAsk * premium), q_ask, stock, "sell", "limit")
             sellPrice = sellOrder.get('price')
             sellID = sellOrder.get('id')
         
-            print "placed sold ord. - %d units at %d ID %d" % (q_ask, sellPrice, sellID)
+            print "SSSSS placed sold ord. - %d units at %d ID %d" % (q_ask, sellPrice, sellID)
 
         end = time.time()
 
 except KeyboardInterrupt:
     print "ctrl+c pressed!"
-    # try to loop through to see those orders.
-
-    with open("results.txt", "w") as EndLog:
-        json.dump(orderIDList, EndLog)
+    EndLog = open("results.txt", "w")
+    json.dump(orderIDList, EndLog)
     EndLog.close()
--- gamemaster.py
+++ gamemaster.py
@@ -10,22 +10,24 @@ class GameMaster:
     gm_url = "https://www.stockfighter.io/gm"
 
     def __init__(self):
+        """to be worked on."""
         pass
 
-    def check_if_instance_is_active(self,instanceID):
-        isTrue = False
+    def check_if_instance_is_active(self, instanceID):
+        "returns the state of instance."
+        is_true = False
         header = {'X-Starfighter-Authorization': self.get_api_key()}
         full_url = "%s/instances/%s" % (self.gm_url, instanceID)
-        response = requests.get(full_url, headers = header)
-        isTrue = response.json().get("state")
-        return isTrue
+        response = requests.get(full_url, headers=header)
+        is_true = response.json().get("state")
+        return is_true
             
     def restart_level(self, instanceID):
         """ restart but keep the same info."""
         
         header = {'X-Starfighter-Authorization': self.get_api_key()}
         full_url = "%s/instances/%s/restart" % (self.gm_url, instanceID)
-        response = requests.post(full_url, headers = header)
+        response = requests.post(full_url, headers=header)
         print "****In restart_level****"
         # print response.json()
         return response
@@ -39,7 +41,7 @@ class GameMaster:
         """returns a list of instances, not working yet"""
         header = {'X-Starfighter-Authorization': self.get_api_key}
         full_url = "%s/levels" % (self.gm_url)
-        response = requests.get(full_url, headers = header)
+        response = requests.get(full_url, headers=header)
 
         try:
             return response.text
@@ -49,22 +51,24 @@ class GameMaster:
     def post_level(self, level):
         header = {'X-Starfighter-Authorization': self.get_api_key()}
         full_url = "%s/levels/%s" % (self.gm_url, level)
-        response = requests.post(full_url, headers = header)
+        response = requests.post(full_url, headers=header)
         try:
             # print response.json()
-            self.write_to_setting(response.json()) # for retrieving later
+            # self.write_to_setting(response.json())  # for retrieving later
             return response
         except ValueError as e:
             return{'error': e, 'raw_content': response.content}
-
+    
+    """
     def write_to_setting(self, responseJSON):
-        """
-            store the current session into file to retrieve
-            for restart, resume etc. 
-        """
+            # store the current session into file to retrieve
+            # for restart, resume etc. 
+
         with open("currentInfo.json", "w") as settings:
-            json.dump(responseJSON,settings)
+            json.dump(responseJSON, settings)
         settings.close()
+    """
+    
 
 class StockFighter:
     """Contains the methods neccessary to make API calls."""
@@ -75,16 +79,18 @@ class StockFighter:
         # look in the currentinfo, if exists check if its active
         # if active prompt user if they want to restart or new
         
+        """
         info = open("currentInfo.json", "r")
-        currentInfo = json.load(info)
-        x = raw_input("Would you like to restart a level? Y/N ->")
+        current_info = json.load(info)
+        x = raw_input("Would you like to restart previous stored level? Y/N ->")
 
-        if currentInfo["ok"] and x.upper() == "Y": 
-            PrevInstanceID = currentInfo["instanceId"]
-            response = gm.restart_level(PrevInstanceID)
+        if current_info["ok"] and x.upper() == "Y":
+            prev_id = current_info["instanceId"]
+            response = gm.restart_level(prev_id)
             info.close()
         else:
-            response = gm.post_level(LevelName)
+        """
+        response = gm.post_level(LevelName)
 
         self.base_url = "https://api.stockfighter.io/ob/api"
         self.account = response.json().get("account")
@@ -92,19 +98,19 @@ class StockFighter:
         self.venues = ''.join(response.json().get("venues"))
         self.tickers = ''.join(response.json().get("tickers"))
         self.header = {'X-Starfighter-Authorization': apikey}
-        print "venue is %s , account is %s, id %s, ticker %s" %(self.venues,self.account,self.instanceID,self.tickers)
+        print "venue is %s , account is %s, id %s, ticker %s" % (self.venues, self.account, self.instanceID, self.tickers)
         # further research at https://discuss.starfighters.io/t/the-gm-api-how-to-start-stop-restart-resume-trading-levels-automagically/143
 
     def status_for_all_orders_in_stock(self, s):
         """ retrieve all orders given account """
         full_url = "%s/venues/%s/accounts/%s/stocks/%s/orders" % (self.base_url, self.venues, self.account, s)
-        response = requests.get(full_url, headers = self.header)
+        response = requests.get(full_url, headers=self.header)
         return response
 
     def get_order_book(self, s):
         """retrieve the order book"""
         full_url = "%s/venues/%s/stocks/%s" % (self.base_url, self.venues, s)
-        response = requests.get(full_url, headers = self.header)
+        response = requests.get(full_url, headers=self.header)
         return response
 
     def read_orderbook(self, oBook, direction, type):
@@ -116,32 +122,31 @@ class StockFighter:
         return best_result
 
     def update_open_orders(self, orderList):
-        """loops through the open ids and check them see 
-        how many have been filled. i am changing it to loop through the orders from 
+        """loops through the open ids and check them see.
+        how many have been filled. i am changing it to loop through the orders from.
         status_for_all_orders_in_stock
         """
         currentPos = 0
         currentPosCash = 0
-
+        expectedPos = 0
         for x in orderList["orders"]:
-            id = x["id"]
+            # o_id = x["id"]
             totalFilled = x["totalFilled"]
+            originalQty = x["originalQty"]
             direction = x["direction"]
             price = x["price"]
-            # print "ID %s, Filled %s, Dirction %s" %(id, totalFilled, direction)  
-
-            
-            # num, direction, state, price = self.fill_confirmation(self.tickers, x)
+           
             if direction == "sell":
                 totalFilled = totalFilled * -1
-
+                originalQty = originalQty * -1
+                
             currentPos += totalFilled
+            expectedPos += originalQty
             # -.01 because we are getting the correct unit
             currentPosCash += totalFilled * price * (-.01)
 
         # print "current pos is %d and $%d" %(currentPos, currentPosCash)
-        return currentPos, currentPosCash
-
+        return currentPos, currentPosCash, expectedPos
 
     def make_order(self, p, q, s, direction, orderType):
         order = {
@@ -154,13 +159,13 @@ class StockFighter:
             "orderType": orderType
         }
         full_url = "%s/venues/%s/stocks/%s/orders" % (self.base_url, self.venues, s)
-        response = requests.post(full_url, headers = self.header, data=json.dumps(order))
+        response = requests.post(full_url, headers=self.header, data=json.dumps(order))
         return response.json()
 
     def get_quote(self, s):
-        # Get last quote 
+        # Get last quote.
         full_url = "%s/venues/%s/stocks/%s/quote" % (self.base_url, self.venues, s)
-        response = requests.get(full_url, headers = self.header)
+        response = requests.get(full_url, headers=self.header)
         last = response.json()
         return last
 
