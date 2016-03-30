451dd06d92dfb84f78a4229b5f5613ecef87d384
Andy Y <andyyao3@gmail.com>
2016-03-30 14:34:03 -0700

    Added execution socket to gamemaster

--- SecondTrade.py
+++ SecondTrade.py
@@ -1,17 +1,15 @@
 import gamemaster
 import time
 import datetime
-import random
 
 start = time.time()
 end = time.time()
 totalGoal = 100000
-positionSoFar = 0
+
 
 sf = gamemaster.StockFighter("chock_a_block")
 # sf = gamemaster.StockFighter.test_mode()
 stock = sf.tickers
-orders = {} # Execution Socket will populate this dictionary.
 
 def identify_unfilled_orders(orderList, callback):
         """check through orderIDlist, and return a list of
@@ -49,48 +47,11 @@ def should_cancel_unfilled(order):
     else:
         return True
     return False
-"""
-def ExecutionSocket(m):
-    currentPos = 0
-    currentPosCash = 0
-    expectedPos = 0
-    global orders
-    print "\n***** IN websocket ****"
-    
-    if m is not None:
-        orders[m["standingId"]] = m
-        
-        for x in orders:
-            o = orders[x]
-            direction = o["order"]["direction"]
-            totalFilled = o["order"]["totalFilled"]
-            price = o["order"]["price"]
-            qty = o["order"]["qty"] + totalFilled
-            
-            if direction == "sell":
-                totalFilled = totalFilled * -1
-                qty = qty * -1
-            
-            currentPos += totalFilled
-            expectedPos += qty
-                # -.01 because we are getting the correct unit
-            currentPosCash += totalFilled * price * (-.01)        
-            
-        
-        print "EXECUTION current pos is %d and $%d" %(currentPos, currentPosCash)
-        # return currentPos, currentPosCash, expectedPos
- 
-    else:
-        print "...restarting websocket..."
-        sf.execution_venue_ticker(ExecutionSocket)
-"""
 
 sf.execution_venue_ticker(sf.execution_socket)
-while totalGoal > positionSoFar and sf.heartbeat():
-    randSleep = random.randint(1,3)
-    time.sleep(randSleep)
+while totalGoal > sf.positionSoFar and sf.heartbeat():
     oBook = sf.get_order_book(stock)
-    bestBid = sf.read_orderbook(oBook, "bids", "price", 1 ) + 1
+    bestBid = sf.read_orderbook(oBook, "bids", "price", 1) + 1
     q_ask = sf.read_orderbook(oBook, "asks", "qty", 1)
     q_ask_actual = int(q_ask * .6)  # i am ordering 20% of actual quote because dont want to affect market.
     
@@ -99,18 +60,19 @@ while totalGoal > positionSoFar and sf.heartbeat():
         # loop through the gapPercent and make multiple bids.
         increment = int(bestBid * .01 * -1)
         worstBid = int(bestBid * (1 - .03))
-        q_increment = int(q_ask_actual* .01)
+        q_increment = int(q_ask_actual * .01)
         q_actual = q_ask_actual
         # print q_actual, bestBid, worstBid, increment
         for actualBid in range(bestBid, worstBid, increment):
             
             buyOrder = sf.make_order(actualBid, q_actual, stock, "buy", "limit")
             print "\n\tBBBBB placed buy ord. +%d units at %d ID %d" % (q_actual, buyOrder.get('price'),
-                                                                        buyOrder.get('id'))
+                                                                       buyOrder.get('id'))
             q_actual -= q_increment
 
+    # end = time.time()
+    # positionSoFar, cash, expectedPosition = sf.update_open_orders(orderIDList)
+    # print "T%d Actual Pos. %d" % ((end - start), positionSoFar)
+    time.sleep(3)
     orderIDList = sf.status_for_all_orders_in_stock(stock)
-    positionSoFar, cash, expectedPosition = sf.update_open_orders(orderIDList)
-    end = time.time()
     identify_unfilled_orders(orderIDList.json(), should_cancel_unfilled)
-    print "T%d Actual Pos. %d" % ((end - start), positionSoFar)
--- thirdTrade.py
+++ thirdTrade.py
@@ -23,8 +23,8 @@ end = time.time()
 sf = gamemaster.StockFighter("sell_side")
 stock = sf.tickers
 
-positionSoFar = 0       # if negative means short if positive long total filled
-expectedPosition = 0    # this is if both filled and to be filled are accounted for (cant be over +/-1000)
+# positionSoFar = 0       # if negative means short if positive long total filled
+# expectedPosition = 0    # this is if both filled and to be filled are accounted for (cant be over +/-1000)
 premium = 1.025
 ma_20_list = []    # moving average 20 lets the script know current trend.
 ma_20 = 0
@@ -44,20 +44,18 @@ def sell_condition(tSpread, tExpectedPosition):
         return True
     return False
 
-# while (end - start) < 120:
 try:
+    sf.execution_venue_ticker(sf.execution_socket)  # start the thread for update the status.
     while nav < 12000:
         orderIDList = sf.status_for_all_orders_in_stock(stock)
-        positionSoFar, cash, expectedPosition = sf.update_open_orders(orderIDList.json())
+        # sf.positionSoFar, sf.cash, sf.expectedPosition = sf.update_open_orders(orderIDList.json())
         oBook = sf.get_order_book(stock)
 
-        BestAsk = sf.read_orderbook(oBook, "asks", "price")
-        BestBid = sf.read_orderbook(oBook, "bids", "price")
-        q_bid = min(sf.read_orderbook(oBook, "bids", "qty"), 250)  # min of 250 because we only have 1000 on either side of pos to work with.
-        q_ask = min(sf.read_orderbook(oBook, "asks", "qty"), 250)
-
+        BestAsk = sf.read_orderbook(oBook, "asks", "price", 1)
+        BestBid = sf.read_orderbook(oBook, "bids", "price", 1)
+        q_bid = min(sf.read_orderbook(oBook, "bids", "qty", 1), 250)  # min of 250 because we only have 1000 on either side of pos to work with.
+        q_ask = min(sf.read_orderbook(oBook, "asks", "qty", 1), 250)
         Difference = BestAsk - BestBid
-
         try:
             if BestBid > 0:
                 Spread = "{0:%}".format(Difference / (BestAsk + 0.0))
@@ -77,21 +75,19 @@ try:
             ma_20 += x + 0.0
         ma_20 = int(ma_20 / curr_count)
      
-        nav = cash + positionSoFar * sf.get_quote(stock).get("last") * (.01)
+        nav = sf.cash + sf.positionSoFar * sf.get_quote(stock).get("last") * (.01)
         nav_currency = '${:,.2f}'.format(nav)   # look prettier in the output below
         
-        print "T%d Pos. %d, Expected Pos. %d Best Ask %r , Best Bid %r, Spread %r, average %r, NAV %s" % \
-            ((end - start), positionSoFar, expectedPosition, BestAsk, BestBid, Spread, ma_20, nav_currency)
+        print "\tT%d, Best Ask %r , Best Bid %r, average %r, \n\tNAV %s" % \
+            ((end - start), BestAsk, BestBid, ma_20, nav_currency)
 
-        time.sleep(2)  # slow things down a bit, because we are querying the same information.
-
-        if buy_condition(Spread, expectedPosition):
+        if buy_condition(Spread, sf.expectedPosition):
             buyOrder = sf.make_order(BestBid, q_bid, stock, "buy", "limit")
             buyPrice = buyOrder.get('price')
             buyID = buyOrder.get('id')
             print "placed buy ord. - %d units at %d ID %d" % (q_bid, buyPrice, buyID)
         
-        if sell_condition(Spread, expectedPosition):
+        if sell_condition(Spread, sf.expectedPosition):
             sellOrder = sf.make_order(int(BestAsk * premium), q_ask, stock, "sell", "limit")
             sellPrice = sellOrder.get('price')
             sellID = sellOrder.get('id')
@@ -99,11 +95,9 @@ try:
             print "placed sold ord. - %d units at %d ID %d" % (q_ask, sellPrice, sellID)
 
         end = time.time()
-
+        time.sleep(2)  # slow things down a bit, because we are querying the same information.
 except KeyboardInterrupt:
     print "ctrl+c pressed!"
-    # try to loop through to see those orders.
-
     with open("results.txt", "w") as EndLog:
         json.dump(orderIDList, EndLog)
     EndLog.close()
