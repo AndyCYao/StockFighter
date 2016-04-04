5bc9c53bb486325e47d9f4c02404a50c9d95c2bc
AndyCYao <andyyao3@gmail.com>
2016-04-03 23:38:48 -0700

    Added charts to FourthTradeMT. CurrentInfoChart uses googlecharts to generate graph.html for a line chart of the queue websocket results

--- CurrentInfoChart.py
+++ CurrentInfoChart.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 
 import gviz_api
-
+import json
+import datetime
 page_template = """
 <html>
   <script src="https://www.google.com/jsapi" type="text/javascript"></script>
@@ -23,33 +24,33 @@ page_template = """
 """
 
 def main():
-  # Creating the data - based on this below
-  # https://developers.google.com/chart/interactive/docs/gallery/linechart#curving-the-lines
-  """
-  description = {"time": ("date", "time"),
-                 "lastQuote": ("number", "lastQuote"),
-                }
-  data = [{"time": "Mike", "salary": (10000, "$10,000"), "full_time": True},
-          {"time": "Jim", "salary": (800, "$800"), "full_time": False},
-          {"time": "Alice", "salary": (12500, "$12,500"), "full_time": True},
-          {"time": "Bob", "salary": (7000, "$7,000"), "full_time": True}]
+    # Creating the data - based on this below
+    # https://developers.google.com/chart/interactive/docs/gallery/linechart#curving-the-lines
+      
+    description = {"quoteTime": ("string", "QuoteTime"),"last": ("number", "Last Traded Price")}
+    data = []
+    file = json.loads(open("currentInfo.json").read())
+    for x in file:
+        file_dict = {"quoteTime": x["quoteTime"], "last": x["last"]}
+        data.append(file_dict)
 
-  # Loading it into gviz_api.DataTable
-  data_table = gviz_api.DataTable(description)
-  data_table.LoadData(data)
-
-  # Create a JavaScript code string.
-  jscode = data_table.ToJSCode("jscode_data",
-                               columns_order=("name", "salary", "full_time"),
-                               order_by="salary")
-  # Create a JSON string.
-  # json = data_table.ToJSon(columns_order=("name", "salary", "full_time"),
-                           # order_by="salary")
-
-  # Put the JS code into the template.
-  print "Content-type: text/html" 
-  print page_template % vars()
-  """
-
+    # need a list of dictionaries.
+    # print data
+    # Loading it into gviz_api.DataTable
+    data_table = gviz_api.DataTable(description)
+    data_table.LoadData(data)
+    
+    # Create a JavaScript code string.
+    jscode = data_table.ToJSCode("jscode_data",
+                                columns_order=("quoteTime", "last"),
+                                order_by="quoteTime")
+    
+    # Put the JS code into the template.
+    print "Content-type: text/html\nLoading graph.html...", 
+    open("Graph.html", 'w').close()  # doing this clears everything first
+    with open("Graph.html", "a") as graph:
+        graph.write(page_template % vars())
+    graph.close()
+    print "Done"
 if __name__ == '__main__':
   main()
